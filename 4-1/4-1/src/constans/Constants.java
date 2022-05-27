647511027e81e2a6ade73dc64b74db8b62b4eaaf
miu-ichinohe <98650389+miu-ichinohe@users.noreply.github.com>
2022-05-27 14:15:07 +0900

    Update Constants.java

--- 4-1/4-1/src/constans/Constants.java
+++ 4-1/4-1/src/constans/Constants.java
@@ -2,7 +2,9 @@ package constans;
 
 	public class Constants {
 
-	    //定数（名前）
+	    /*定数（名前）　= 定数とは固定の値で、変数が代入することで値を変化させることができるのに対して、定数は値が固定なので一度値を代入すると変更させることができません
+	      固定で使いたい値がある場合に使います。
+	    */
 	    public static final String CHECK_CLASS_JAVA = "java吉";
 	    public static final String CHECK_CLASS_HOGE = "hoge";
 	    public static final String CHECK_CLASS_R2D2 = "R2D2";
