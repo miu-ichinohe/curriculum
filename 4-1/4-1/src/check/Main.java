d9011824d600faa9d37fd6e535a981c3b48595a5
miu-ichinohe <98650389+miu-ichinohe@users.noreply.github.com>
2022-09-26 07:38:34 +0900

    Update Main.java

--- 4-1/4-1/src/check/Main.java
+++ 4-1/4-1/src/check/Main.java
@@ -7,10 +7,11 @@ import constans.Constants;
 
 public class Main {
 	
-
+	//メインメソッド
 	public static void main(String[] args) {
-		//printNameメソッド
+		//printNameメソッド実行　引数として一戸、美羽を渡す
 		printName ("一戸" , "美羽");
+		
 		
 		Pet pet = new Pet (Constants.CHECK_CLASS_JAVA , Constants.CHECK_CLASS_HOGE);
 		pet. introduce(); 
@@ -19,7 +20,10 @@ public class Main {
 		robotPet .introduce();
 	}
 	
+	//printNameメソッド
+	//渡された引数を受け取っている　　String（文字列）　のfirstName→一戸　と　lastName→美羽
 	private static void printName(String firstName , String lastName) {
+		//printNameメソッド + ⇨ firstName(一戸)　lastName（美羽）　をコンソールに出力
 		System.out.println("printNameメソッド" + " ⇨ " + firstName + lastName);
 	}
 	
