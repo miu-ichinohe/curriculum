08bb6e6fa121139b3529531e10552ffca11e04bc
miu-ichinohe <98650389+miu-ichinohe@users.noreply.github.com>
2022-10-12 12:22:56 +0900

    Update Pet.java

--- 4-1/4-1/src/check/Pet.java
+++ 4-1/4-1/src/check/Pet.java
@@ -4,7 +4,9 @@ public class Pet {
 
 	    private String name;
 	    private String masterName;
-
+	
+	    //コンストラクタ
+	　　　　　　 //①アクセス修飾子②コンストラクタ名（クラス名と同じにする）（③引数の型、④引数名）
 	    public Pet(String name, String masterName) {
 	           //↓５行目のname = 引数のname
 		this.name = name;
@@ -24,7 +26,6 @@ public class Pet {
 	        System.out.println("■ご主人様は" + masterName + "です");
 	    }
 	}
-	//
 	class RobotPet extends Pet {
 	    public RobotPet(String name, String masterName) {
 	        super(name, masterName);
